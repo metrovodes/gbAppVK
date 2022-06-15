44872ae32d52f2b181c981ff31a8f75f67d142f0
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-06-15 20:44:09 +0300

    Init commit

---
+++ gbAppVK/gbAppVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gbAppVK
+//
+//  Created by Артем Седов on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
