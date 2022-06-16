a2dd97b2fed7184db2c84fe3243b3cce355956b7
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-06-16 09:44:20 +0300

    Outlet для текстовых полей и кнопки. Action для кнопки.

--- gbAppVK/gbAppVK/ViewController.swift
+++ gbAppVK/gbAppVK/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func loginButton(_ sender: Any) {
+        print(#function)
+        if loginField.hasText {
+            print(loginField.text!)
+        }
+    }
 }
 
