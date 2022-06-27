a7302e36252ad75f15aba9399ed1d7bac673a920
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-06-27 15:24:48 +0300

    Исправление ошибок

---
+++ gbAppVK/gbAppVK/AvatarCell.swift
@@ -0,0 +1,14 @@
+//
+//  AvatarCell.swift
+//  gbAppVK
+//
+//  Created by Артем Седов on 27.06.2022.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+}
